78f8ee620641e68a43a82ac880155d1a7469a9a9
Nicola Strappazzon <nicola51980@gmail.com>
2015-05-28 13:26:46 +0200

    Update help message

--- mysqlexportslowlog.sh
+++ mysqlexportslowlog.sh
@@ -3,9 +3,9 @@
 usage()
 {
   cat << EOF
-  usage: $0 -h host -u user -p password -d foo
+  usage: $0 -h host -u user -p password -d foo > slow.log
 
-  $0 -h 127.0.0.1 -u root -p admin -d foo
+  $0 -h 127.0.0.1 -u root -p admin -d foo > slow.log
 
   OPTIONS:
      -h Host
